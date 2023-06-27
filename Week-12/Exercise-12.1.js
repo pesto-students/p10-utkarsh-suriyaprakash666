55295b4fba07ef06a9edb2e0774ff97cfeeabb54
Suriya <suriyaprakash.s666@gmail.com>
2023-06-27 10:07:32 +0530

    Completed Exercise 12.1

---
+++ Week-12/Exercise-12.1.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    }
+    let temp = this.root;
+    while (true) {
+      if (newNode.value === temp.value) return undefined;
+      if (newNode.value < temp.value) {
+        if (temp.left === null) {
+          temp.left = newNode;
+          return this;
+        }
+        temp = temp.left;
+      } else {
+        if (temp.right === null) {
+          temp.right = newNode;
+          return this;
+        }
+        temp = temp.right;
+      }
+    }
+  }
+
+  depth(temp = this.root) {
+    if (temp === null) return 0;
+    let leftDepth = this.depth(temp.left);
+    let rightDepth = this.depth(temp.right);
+
+    return Math.max(leftDepth, rightDepth);
+  }
+}
+
+const myTree = new BST();
+myTree.insert(47);
+myTree.insert(21);
+myTree.insert(76);
+myTree.insert(18);
+myTree.insert(27);
+myTree.insert(52);
+myTree.insert(82);
+
+console.log(myTree);
+console.log(myTree.depth());
